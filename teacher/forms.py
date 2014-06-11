c8982d4081f18add2b4f2a5093d84d9e8b4f15ae
shenlian <1019947054@qq.com>
2014-06-11 16:38:54 +0800

    add project budget information

--- teacher/forms.py
+++ teacher/forms.py
@@ -1,1 +1,7 @@
 # coding: UTF-8
+from django import forms
+
+class ProjectBudgetInformationForm(forms.Form):
+	project_basicexpenses = forms.CharField(required=True,label="基本科研业务费经费",widget=forms.TextInput(attrs={'class':'',}))
+	project_selfexpense = forms.CharField(required=True,label="项目单位自筹经费",widget=forms.TextInput(attrs={'class':'',}))
+	project_totalexpense = forms.CharField(required=True,label="总经费",widget=forms.TextInput(attrs={'class':'',}))
--- teacher/views.py
+++ teacher/views.py
@@ -5,7 +5,7 @@ Created on 2014-06-07
 Desc: teacher' view, includes home(manage), review report view
 '''
 from django.shortcuts import render
-
+from teacher.forms import ProjectBudgetInformationForm
 def homeView(request):
     context={}
     return render(request,"teacher/home.html",context)
@@ -13,3 +13,17 @@ def homeView(request):
 def final_report_view(request):
     context = {}
     return render(request,"teacher/final.html",context)
+
+def financial_view(request):
+    budgetinfoform = ProjectBudgetInformationForm()
+    if request.method == "POST":
+        budgetinfoform = ProjectBudgetInformationForm(request.POST)
+        if budgetinfoform.is_valid():
+            budgetinfo = budgetinfoform.cleaned_data
+            print budgetinfo['project_basicexpenses']
+    else:
+        budgetinfoform = ProjectBudgetInformationForm()
+    context = {
+        'budgetinfoform':budgetinfoform,
+    }
+    return render(request,"teacher/financial.html",context)
