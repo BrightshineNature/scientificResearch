0f4711cbee8e3a634519ba531bad6a4c78e02a3e
liuzhuocpp <liuzhuocpp@qq.com>
2014-06-16 17:41:03 +0800

    gogo

--- teacher/urls.py
+++ teacher/urls.py
@@ -12,12 +12,14 @@ urlpatterns = patterns('',
     url(
         r'^$',
         teacher_views.homeView,
-       ),(
+       ),
+    url(
         r'^memberchange$',
         teacher_views.memberChange,
-    )      
+    
     ),
-	url(
+	
+    url(
 		r'final$',
 		teacher_views.final_report_view,
 	),
