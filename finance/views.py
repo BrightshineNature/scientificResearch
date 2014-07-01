4a92d72edfb6f081e074b5f61262445414e58ac3
shenlian <1019947054@qq.com>
2014-07-01 10:58:36 +0800

    add financial function in finance

--- finance/views.py
+++ finance/views.py
@@ -5,8 +5,26 @@ Created on 2014-06-07
 Desc: adminStaff' view, includes home(manage), review report view
 '''
 from django.shortcuts import render
+from common.views import scheduleManage, financialManage
+from teacher.forms import ProjectBudgetInformationForm,ProjectBudgetAnnualForm
 
 def homeView(request):
 
     context={}
     return render(request,"finance/home.html",context)
+
+def financialView(request):
+    userauth = {
+                "role": 'finance',                
+    }
+    return financialManage(request, userauth)
+
+
+def financialInfoView(request):
+    budgetinfoform = ProjectBudgetInformationForm()
+    budgetannuform = ProjectBudgetAnnualForm()    
+    context = {
+        'budgetinfoform':budgetinfoform,
+        'budgetannuform':budgetannuform,
+    }
+    return render(request,"finance/project_financial_info.html",context)
