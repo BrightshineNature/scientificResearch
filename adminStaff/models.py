bf68c654ef4a5e0106f193610ce84b0994e4ecf6
liuzhuocpp <liuzhuocpp@qq.com>
2014-07-19 10:15:55 +0800

    add school_user

--- adminStaff/models.py
+++ adminStaff/models.py
@@ -10,7 +10,7 @@ import datetime
 
 class Special(models.Model):
 
-    
+    school_user = models.ForeignKey(SchoolProfile, blank=True, null=False, verbose_name=u"专题管理员")    
     name = models.CharField(blank=False,max_length=30)
     def __unicode__(self):
         return self.name
