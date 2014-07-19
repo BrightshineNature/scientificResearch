eacaec494ed842c3e3218cb8d71a259bc9fe731d
DutBright <757289247@qq.com>
2014-07-19 20:28:33 +0800

    add the registion

---
+++ 1.py
@@ -0,0 +1,4 @@
+# coding: UTF-8
+from registration.models import RegistrationManager
+from const import *
+RegistrationManager().create_inactive_user("a","ttt","liuye","1","757289247@qq.com",TEACHER_USER)
