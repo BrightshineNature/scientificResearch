91029e10b1c46b4fba590ac0f405e120f31a5354
JunHu <hujun09012@gmail.com>
2014-07-26 02:06:16 +0800

    delete

--- media/js/teacher/setting_control.js
+++ media/js/teacher/setting_control.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     if($("#alert-bar").length == 0){
-        alert("alert-bar");
         if($("#id_name").val() && $("#id_card").val()) {
             $("#id_name").attr("readonly", "true");
             $("#id_card").attr("readonly", "true");
