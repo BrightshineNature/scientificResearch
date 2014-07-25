28c3ef76a0131df30b70a4dc0b459f425e64a6dc
JunHu <hujun09012@gmail.com>
2014-07-25 16:02:20 +0800

    add special in search bar

--- school/forms.py
+++ school/forms.py
@@ -1,19 +1,28 @@
 # coding: UTF-8
 
 from django import forms
-from users.models import College
+from users.models import College, Special
 
-class CollegeForm(forms.Form):
+class FilterForm(forms.Form):
     college_list = College.objects.all()
     COLLEGE_CHOICE = [(-1, "所有学院")]
     COLLEGE_CHOICE.extend([(college.id, college.name) for college in college_list])
     COLLEGE_CHOICE = tuple(COLLEGE_CHOICE)
     colleges = forms.ChoiceField(required = True, choices = COLLEGE_CHOICE, widget = forms.Select(attrs = {"class": "form-control",}))
+
+    SPECIAL_CHOICE = tuple((special.id, special.name) for special in Special.objects.all())
+    specials = forms.ChoiceField(required = True, choices = SPECIAL_CHOICE, widget = forms.Select(attrs = {"class": "form-control",}))
+
     def __init__(self, *args, **kwargs):
-        super(CollegeForm, self).__init__(*args, **kwargs)
+        super(FilterForm, self).__init__(*args, **kwargs)
         college_list = College.objects.all()
         COLLEGE_CHOICE = [(-1, "所有学院")]
         COLLEGE_CHOICE.extend([(college.id, college.name) for college in college_list])
         COLLEGE_CHOICE = tuple(COLLEGE_CHOICE)
 
         self.fields["colleges"].choices = COLLEGE_CHOICE
+       
+        SPECIAL_CHOICE = tuple((special.id, special.name) for special in Special.objects.all())
+        specials = forms.ChoiceField(required = True, choices = SPECIAL_CHOICE, widget = forms.Select(attrs = {"class": "form-control",}))
+
+
