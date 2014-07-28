0ffc5d5b4acb7cb81a05641dde410d84a7a34139
JunHu <hujun09012@gmail.com>
2014-07-28 23:17:56 +0800

    add expert home page

--- expert/views.py
+++ expert/views.py
@@ -6,10 +6,22 @@ Desc: expert' view, includes home(manage), review report view
 '''
 from django.shortcuts import render
 from common.forms import ProjectInfoForm, BasisContentForm, BaseConditionForm
+from django.db.models import Q
+
+from adminStaff.models import Re_Project_Expert
+from users.models import ExpertProfile
+from backend.utility import getContext
 
 def homeView(request):
-    context={}
-    return render(request,"expert/home.html",context)
+    expert = ExpertProfile.objects.get(userid = request.user)
+    project_list_1 = [re_obj.project for re_obj in Re_Project_Expert.objects.filter(Q(expert = expert) & Q(is_first_round = True))]
+    project_list_2 = [re_obj.project for re_obj in Re_Project_Expert.objects.filter(Q(expert = expert) & Q(is_first_round = False))]
+    context = {}
+
+    context.update(getContext(project_list_1, 1, "item", 0, 2))
+    context.update(getContext(project_list_2, 1, "item2", 0, 2))
+
+    return render(request,"expert/home.html", context)
 
 def finalReportView(request):
     context = {}
