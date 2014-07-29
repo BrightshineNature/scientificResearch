e9e494a860313f29bd7b2443a79e7acba0f00c78
DutBright <757289247@qq.com>
2014-07-29 19:44:52 +0800

    modify user model

--- common/sendEmail.py
+++ common/sendEmail.py
@@ -4,16 +4,18 @@ from const.models import UserIdentity
 from registration.models import RegistrationManager
 def sendemail(request,username,password,email,identity,person_firstname,send_email=True, **kwargs):
     #判断用户名是否存在存在直接返回
-    if not AuthUserExist(username, identity):
+    if not AuthUserExist(username, identity,person_firstname):
         RegistrationManager().create_inactive_user(request,username,password,email,identity,person_firstname,send_email,**kwargs)
         return True
     else:
         return False
-def AuthUserExist(username, identity):
+def AuthUserExist(username, identity,person_firstname):
     if User.objects.filter(username=username).count():
         user_obj = User.objects.get(username=username)
         ui_obj = UserIdentity.objects.get(identity=identity)
         if ui_obj.auth_groups.filter(id=user_obj.id).count():
+            return True
+        elif user_obj.first_name != person_firstname:
             return True
         else:
             return False
