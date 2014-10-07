0847d617134e675f3d2a1332fe8178ad5dfce916
shenlian <1019947054@qq.com>
2014-10-07 20:34:30 +0800

    add valid check in fundbudget

--- teacher/utility.py
+++ teacher/utility.py
@@ -25,3 +25,24 @@ def copyFundsummaryToBudget(pid):
     fundbudget.expertadvice_budget = fundsummary.expertadvice_budget
     fundbudget.total_budget = fundsummary.total_budget
     fundbudget.save()
+
+def copyBudgetToFundsummary(pid):
+    fundsummary = ProjectFundSummary.objects.get(project_id = pid)
+    fundbudget = ProjectFundBudget.objects.get(project_id = pid)
+    loginfo(p=fundsummary.total_budget,label = "fundsummary")
+    loginfo(p=fundbudget.total_budget,label = "fundbudget")
+    fundsummary.equcosts_budget = fundbudget.equcosts_budget 
+    fundsummary.equacquisition_budget = fundbudget.equacquisition_budget  
+    fundsummary.equtrial_budget = fundbudget.equtrial_budget
+    fundsummary.equrent_budget = fundbudget.equrent_budget
+    fundsummary.material_budget  = fundbudget.material_budget 
+    fundsummary.testcosts_budget = fundbudget.testcosts_budget
+    fundsummary.fuelpower_budget  = fundbudget.fuelpower_budget 
+    fundsummary.travel_budget = fundbudget.travel_budget
+    fundsummary.conference_budget  = fundbudget.conference_budget 
+    fundsummary.cooperation_budget  = fundbudget.cooperation_budget 
+    fundsummary.publish_budget  = fundbudget.publish_budget 
+    fundsummary.laborcosts_budget = fundbudget.laborcosts_budget 
+    fundsummary.expertadvice_budget = fundbudget.expertadvice_budget
+    fundsummary.total_budget = fundbudget.total_budget
+    fundsummary.save()
