7556de67640f696c65cac9e63b0f2111030b3adb
liuzhuocpp <liuzhuocpp@qq.com>
2014-11-04 17:08:10 +0800

    fix validation

--- media/js/common/application.js
+++ media/js/common/application.js
@@ -81,7 +81,7 @@ function saveProjectMemberCallback(data){
             $('#project_member_form_error').show(500);
             return ;
         }
-        $('#project_member_form_error').html('<h3>您有字段没有填写。</h3>');
+        $('#project_member_form_error').html('<h3>您有字段没有填写或填写错误。</h3>');
         $('#project_member_form_error').hide();
         $('#project_member_form_error').show(500);
 
@@ -212,7 +212,6 @@ $(document).on("click", ".save_button",function(){
     var pid = $(this).parents("[pid]").attr("pid");
     if($(cnt_content).attr("id") == "project_info")
     {
-    
         Dajaxice.common.saveProjectInfoForm(saveProjectInfoFormCallback,{
             'form': $("#project_info_form").serialize(),
             'pid': pid,
@@ -297,6 +296,12 @@ function saveProjectInfoFormCallback(data)
         }
 
         $("#project_info_form_error").html("<h3>您有字段没有被填写。</h3>");
+        $("#project_info_form_error").hide();
+        $("#project_info_form_error").show(500);
+    }
+    else if(data.status == 2)
+    {
+        $("#project_info_form_error").html("<h3>!错误:“研究开始时间“大于“研究结束时间“</h3>");
         $("#project_info_form_error").hide();
         $("#project_info_form_error").show(500);
     }
