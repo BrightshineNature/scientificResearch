a1cd08d5a9472b248e5bdeefd93ff13200e315d2
DutBright <757289247@qq.com>
2014-11-20 17:46:36 +0800

    add the

--- media/js/home/checkbrowser.js
+++ media/js/home/checkbrowser.js
@@ -2,7 +2,7 @@ $(function getOs(){
   var checkerror=false;
   var u_agent = navigator.userAgent;
   if(u_agent.indexOf("Safari")>-1 || u_agent.indexOf("Firefox")>-1 || window.opera){
-  }else if(u_agent.indexOf("MSIE 6.0")>-1 ||u_agent.indexOf("MSIE 7.0")>-1 ){
-    alert("为了达到最佳浏览效果，我们推荐您使用以下浏览器 请使用'谷歌浏览器'或'火狐浏览器'");
+  }else if(u_agent.indexOf("MSIE 6.0")>-1 ||u_agent.indexOf("MSIE 8.0")>-1 || u_agent.index("MSIE 7.0")>-1){
+    alert("为了达到最佳浏览效果，我们推荐您使用以下浏览器 请使用'谷歌浏览器'或'火狐浏览器',360浏览器");
   }
 })
