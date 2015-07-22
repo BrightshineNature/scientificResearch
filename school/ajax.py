8167a8192c9d974b085279987c168e16de4bf15f
Rosen <rosen9212@gmail.com>
2015-07-22 14:49:10 +0800

    bug fixed

--- school/ajax.py
+++ school/ajax.py
@@ -365,7 +365,10 @@ def ExpertinfoExport(request,special_id,eid,form):
         elif eid == TYPE_FINAL_ALLOC[0]:
             proj_set = ProjectSingle.objects.filter(Q(project_special=special) & Q(project_status__status = PROJECT_STATUS_FINAL_EXPERT_SUBJECT))
         loginfo(proj_set.count())
-        path = get_xls_path(request,special.expert_review.category,proj_set,special.name)
+        if eid==TYPE_ALLOC[0]:
+            path = get_xls_path(request,special.expert_review.category,proj_set,special.name)
+        elif eid == TYPE_FINAL_ALLOC[0]:
+            path = path = get_xls_path(request,special.expert_final_review.category,proj_set,special.name)
         ret = {'path':path}
         return simplejson.dumps(ret)
     return simplejson.dumps({'status':'0'})
